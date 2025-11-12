bba0a4f9a58df2d3fac7d1020059ba5bc41aced1
wasdiezz <vkavelmakher@mail.ru>
2025-11-12 13:24:19 +0300

    tryToCommit

---
+++ src/my_dz_books/Main.java
@@ -0,0 +1,33 @@
+package my_dz_books;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //книги
+
+        Book[] books1 = {
+                new Book("Мастер и Маргарита", "Михаил Булгаков", 480),
+                new Book("Преступление и наказание", "Фёдор Достоевский", 670),
+                new Book("Война и мир", "Лев Толстой", 1225),
+                new Book("Три товарища", "Эрих Мария Ремарк", 430),
+                new Book("1984", "Джордж Оруэлл", 328)
+        };
+
+        Shelf[] shelf = {git add .
+                git commit -m "твой комментарий"
+                new Shelf(books1),
+        };
+
+        Rack[] rack = {
+                new Rack(shelf)
+        };
+
+        Library library = new Library (rack);
+
+        System.out.println(library.totalbooks());
+        System.out.println(library.findBookByTitle("Мастер и Маргарита"));
+        System.out.println(library.getRack(234));
+
+
+    }
+}
